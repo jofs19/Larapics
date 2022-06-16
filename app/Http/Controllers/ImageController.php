c48717318093eacb40daabe73b5ceb88db4a6cce
John <johnoliversantiago5@gmail.com>
2022-06-16 21:06:51 +0800

    initial commit

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Image;
+use Illuminate\Http\Request;
+use App\Http\Requests\ImageRequest;
+
+class ImageController extends Controller
+{
+    public function index()
+    {
+        $images = Image::published()->latest()->paginate(6)->withQueryString();
+        return view('image.index', compact('images'));
+    }
+
+    public function show(Image $image)
+    {
+        return view('image.show', compact('image'));
+    }
+
+    public function create()
+    {
+        return view("image.create");
+    }
+
+    public function store(ImageRequest $request)
+    {
+        Image::create($request->getData());
+        return to_route('images.index')->with('message', "Image has been uploaded successfully");    
+    
+    }
+
+    public function edit(Image $image)
+    {
+        return view("image.edit", compact('image'));
+    }
+
+    public function update(Image $image, ImageRequest $request)
+    {
+        $image->update($request->getData());
+        return to_route('images.index')->with('message', "Image has been updated successfully");
+    }
+
+    public function destroy(Image $image)
+    {
+        $image->delete();
+        return to_route('images.index')->with('message', "Image has been removed successfully");
+    }
+
+}
---
+++ resources/views/image/index.blade.php
@@ -0,0 +1,29 @@
+<x-layout title="Discover free images">
+    <div class="container-fluid mt-4">
+        @if ($message = session('message'))
+            <x-alert type="success" dismissible>
+                {{ $component->icon() }}
+                {{ $message }}
+            </x-alert>
+        @endif
+        <div class="row" data-masonry='{"percentPosition": true }'>
+            @foreach ($images as $image)
+                <div class="col-sm-6 col-lg-4 mb-4">
+                    <div class="card">
+                        <a href="{{ $image->permalink() }}">
+                            <img src="{{ $image->fileUrl() }}" alt="{{ $image->title }}" class="card-img-top">
+                        </a>
+                        <div class="photo-buttons">
+                            <div>
+                                <a class="btn btn-sm btn-info me-2" href="{{ $image->route('edit') }}">Edit</a>                                <x-form action="{{ $image->route('destroy') }}" method="DELETE">
+                                    <button class="btn btn-sm btn-danger" type="submit" onclick="return confirm('Are you sure?')">Delete</button>
+                                </x-form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        {{ $images->links() }}
+    </div>
+</x-layout> 
